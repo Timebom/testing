1921862ae594d2e7d0dceeca3b2aede098156c96
Timebom <101462670+Timebom@users.noreply.github.com>
2025-09-14 13:17:03 +0530

    Update card.dart
    added constructor

--- card.dart
+++ card.dart
@@ -1,3 +1,23 @@
 import 'package:flutter/material.dart';
 
-class GlassCard extends StatefulWidget() {}
+class GlassCard extends StatefulWidget() {
+  const GlassCard({super.key});
+
+  State<GlassCard> contentState() => _GlassCardState();
+}
+
+class _GlassCardState extends State<GlassCard> {
+  List<Text> listText;
+  Padding? padding;
+  Border? borderSize;
+  Color? highlightTint;
+  bool? transparent;
+
+  _GlassCardState({
+    required this.listTest;
+    this.padding = Padding(10,10);
+    this.borderSize = Border.outlineBorder();
+    this.highlightTint = Colors.pink.shad300;
+    this.transparent = false;
+  });
+}
