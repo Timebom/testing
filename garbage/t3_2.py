359e6cce6cd10c75d95ad5b9635dbab8f91cd1da
Timebom <101462670+Timebom@users.noreply.github.com>
2026-03-09 08:46:48 +0530

    Create t3_2.py

---
+++ garbage/t3_2.py
@@ -0,0 +1,7 @@
+#p3_2
+from sklearn.metrics import average_precision_score
+
+y_true = [0,1,1,0,1,1]
+y_scores = [0.1,0.4,0.35,0.8,0.65,0.9]
+average_precision = average_precision_score(y_true, y_scores)
+print(f"Average Precision-Recall Score: {average_precision}")
